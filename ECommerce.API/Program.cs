5dd681e186c8c47159fd3a6f5623650c9b99b313
Fabio Borges <fabioborges.ti@outlook.com>
2022-10-16 13:38:15 -0300

    .

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,7 +1,19 @@
 using ECommerce.API.Repositories;
 using ECommerce.API.Repositories.Interfaces;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region Serilog
+
+IWebHostBuilder webHostBuilder = builder.WebHost.UseSerilog((context, configuration) =>
+{
+    configuration
+        .WriteTo.Console();
+    //.WriteTo.MSSqlServer(context.Configuration["ConnectionString:db"], sinkOptions: new MSSqlServerSinkOptions { AutoCreateSqlTable = true, TableName = "LogAPI" });
+});
+
+#endregion
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
